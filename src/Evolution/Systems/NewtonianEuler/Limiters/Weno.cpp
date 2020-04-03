fc11283bd33f0e961912e3251223924c82632da8
Francois Hebert <fmh33@cornell.edu>
2020-04-03 11:11:08 -0700

    Add WENO limiter specialized to NewtonianEuler

---
+++ src/Evolution/Systems/NewtonianEuler/Limiters/Weno.cpp
@@ -0,0 +1,194 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Evolution/Systems/NewtonianEuler/Limiters/Weno.hpp"
+
+#include <array>
+#include <boost/functional/hash.hpp>  // IWYU pragma: keep
+#include <cstddef>
+#include <unordered_map>
+#include <utility>
+
+#include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Element.hpp"  // IWYU pragma: keep
+#include "Domain/Mesh.hpp"
+#include "Domain/SizeOfElement.hpp"
+#include "Domain/Tags.hpp"  // IWYU pragma: keep
+#include "ErrorHandling/Assert.hpp"
+#include "Evolution/DiscontinuousGalerkin/Limiters/Weno.hpp"
+#include "Evolution/DiscontinuousGalerkin/Limiters/WenoType.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/MeanValue.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace NewtonianEuler {
+namespace Limiters {
+
+template <size_t VolumeDim>
+Weno<VolumeDim>::Weno(const ::Limiters::WenoType weno_type,
+                      const double neighbor_linear_weight,
+                      const double tvb_constant,
+                      const bool disable_for_debugging) noexcept
+    : weno_type_(weno_type),
+      neighbor_linear_weight_(neighbor_linear_weight),
+      tvb_constant_(tvb_constant),
+      disable_for_debugging_(disable_for_debugging) {
+  ASSERT(tvb_constant >= 0.0, "The TVB constant must be non-negative.");
+}
+
+template <size_t VolumeDim>
+// NOLINTNEXTLINE(google-runtime-references)
+void Weno<VolumeDim>::pup(PUP::er& p) noexcept {
+  p | weno_type_;
+  p | neighbor_linear_weight_;
+  p | tvb_constant_;
+  p | disable_for_debugging_;
+}
+
+template <size_t VolumeDim>
+void Weno<VolumeDim>::package_data(
+    const gsl::not_null<PackagedData*> packaged_data,
+    const Scalar<DataVector>& mass_density_cons,
+    const tnsr::I<DataVector, VolumeDim>& momentum_density,
+    const Scalar<DataVector>& energy_density, const Mesh<VolumeDim>& mesh,
+    const std::array<double, VolumeDim>& element_size,
+    const OrientationMap<VolumeDim>& orientation_map) const noexcept {
+  const ConservativeVarsWeno weno(weno_type_, neighbor_linear_weight_,
+                                  tvb_constant_, disable_for_debugging_);
+  weno.package_data(packaged_data, mass_density_cons, momentum_density,
+                    energy_density, mesh, element_size, orientation_map);
+}
+
+template <size_t VolumeDim>
+template <size_t ThermodynamicDim>
+bool Weno<VolumeDim>::operator()(
+    const gsl::not_null<Scalar<DataVector>*> mass_density_cons,
+    const gsl::not_null<tnsr::I<DataVector, VolumeDim>*> momentum_density,
+    const gsl::not_null<Scalar<DataVector>*> energy_density,
+    const gsl::not_null<Scalar<DataVector>*> limiter_diagnostics,
+    const Mesh<VolumeDim>& mesh, const Element<VolumeDim>& element,
+    const std::array<double, VolumeDim>& element_size,
+    const EquationsOfState::EquationOfState<false, ThermodynamicDim>&
+        equation_of_state,
+    const std::unordered_map<
+        std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>, PackagedData,
+        boost::hash<std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>>>&
+        neighbor_data) const noexcept {
+  if (UNLIKELY(disable_for_debugging_)) {
+    // Do not modify input tensors
+    return false;
+  }
+
+  // Enforce restrictions on h-refinement, p-refinement
+  if (UNLIKELY(alg::any_of(element.neighbors(),
+                           [](const auto& direction_neighbors) noexcept {
+                             return direction_neighbors.second.size() != 1;
+                           }))) {
+    ERROR("The Weno limiter does not yet support h-refinement");
+    // Removing this limitation will require:
+    // - Generalizing the computation of the modified neighbor solutions.
+    // - Generalizing the WENO weighted sum for multiple neighbors in each
+    //   direction.
+  }
+  alg::for_each(neighbor_data, [&mesh](const auto& neighbor_and_data) noexcept {
+    if (UNLIKELY(neighbor_and_data.second.mesh != mesh)) {
+      ERROR("The Weno limiter does not yet support p-refinement");
+      // Removing this limitation will require generalizing the
+      // computation of the modified neighbor solutions.
+    }
+  });
+
+  // Checks for the post-timestep, pre-limiter NewtonianEuler state, e.g.:
+  const double mean_density = mean_value(get(*mass_density_cons), mesh);
+  ASSERT(mean_density > 0.0,
+         "Positivity was violated on a cell-average level.");
+  if (ThermodynamicDim == 2) {
+    const double mean_energy = mean_value(get(*energy_density), mesh);
+    ASSERT(mean_energy > 0.0,
+           "Positivity was violated on a cell-average level.");
+  }
+  // End pre-limiter checks
+
+  const ConservativeVarsWeno weno(weno_type_, neighbor_linear_weight_,
+                                  tvb_constant_, disable_for_debugging_);
+  const bool limiter_activated =
+      weno(mass_density_cons, momentum_density, energy_density, mesh, element,
+           element_size, neighbor_data);
+
+  // Checks for the post-limiter NewtonianEuler state, e.g.:
+  ASSERT(min(get(*mass_density_cons)) > 0.0, "Bad density after limiting.");
+  if (ThermodynamicDim == 2) {
+    Scalar<DataVector> pressure{};
+    // Need this make_overloader until we can use `if constexpr`
+    make_overloader(
+        [&pressure,
+         &mass_density_cons](const EquationsOfState::EquationOfState<false, 1>&
+                                 the_equation_of_state) noexcept {
+          pressure =
+              the_equation_of_state.pressure_from_density(*mass_density_cons);
+        },
+        [&pressure, &mass_density_cons, &momentum_density,
+         &energy_density](const EquationsOfState::EquationOfState<false, 2>&
+                              the_equation_of_state) noexcept {
+          const auto specific_internal_energy = Scalar<DataVector>{
+              get(*energy_density) / get(*mass_density_cons) -
+              0.5 * get(dot_product(*momentum_density, *momentum_density)) /
+                  square(get(*mass_density_cons))};
+          pressure = the_equation_of_state.pressure_from_density_and_energy(
+              *mass_density_cons, specific_internal_energy);
+        })(equation_of_state);
+    ASSERT(min(get(pressure)) > 0.0, "Bad energy after limiting.");
+  }
+  // End post-limiter checks
+
+  get(*limiter_diagnostics) = (limiter_activated ? 1.0 : 0.0);
+  return limiter_activated;
+}
+
+template <size_t LocalDim>
+bool operator==(const Weno<LocalDim>& lhs, const Weno<LocalDim>& rhs) noexcept {
+  return lhs.weno_type_ == rhs.weno_type_ and
+         lhs.neighbor_linear_weight_ == rhs.neighbor_linear_weight_ and
+         lhs.tvb_constant_ == rhs.tvb_constant_ and
+         lhs.disable_for_debugging_ == rhs.disable_for_debugging_;
+}
+
+template <size_t VolumeDim>
+bool operator!=(const Weno<VolumeDim>& lhs,
+                const Weno<VolumeDim>& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define THERMO_DIM(data) BOOST_PP_TUPLE_ELEM(1, data)
+
+#define INSTANTIATE(_, data) template class Weno<DIM(data)>;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3))
+
+#undef INSTANTIATE
+
+#define INSTANTIATE(_, data)                                                   \
+  template bool Weno<DIM(data)>::operator()(                                   \
+      const gsl::not_null<Scalar<DataVector>*>,                                \
+      const gsl::not_null<tnsr::I<DataVector, DIM(data)>*>,                    \
+      const gsl::not_null<Scalar<DataVector>*>,                                \
+      const gsl::not_null<Scalar<DataVector>*>, const Mesh<DIM(data)>&,        \
+      const Element<DIM(data)>&, const std::array<double, DIM(data)>&,         \
+      const EquationsOfState::EquationOfState<false, THERMO_DIM(data)>&,       \
+      const std::unordered_map<                                                \
+          std::pair<Direction<DIM(data)>, ElementId<DIM(data)>>, PackagedData, \
+          boost::hash<                                                         \
+              std::pair<Direction<DIM(data)>, ElementId<DIM(data)>>>>&)        \
+      const noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (1, 2))
+
+#undef INSTANTIATE
+#undef DIM
+#undef THERMO_DIM
+
+}  // namespace Limiters
+}  // namespace NewtonianEuler
---
+++ tests/Unit/Evolution/Systems/NewtonianEuler/Limiters/Test_Weno.cpp
@@ -0,0 +1,360 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <boost/functional/hash.hpp>
+#include <cstddef>
+#include <unordered_map>
+#include <utility>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tags.hpp"
+#include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Direction.hpp"
+#include "Domain/Element.hpp"
+#include "Domain/ElementId.hpp"
+#include "Domain/LogicalCoordinates.hpp"
+#include "Domain/Mesh.hpp"
+#include "Evolution/DiscontinuousGalerkin/Limiters/Weno.hpp"
+#include "Evolution/DiscontinuousGalerkin/Limiters/WenoType.hpp"
+#include "Evolution/Systems/NewtonianEuler/Limiters/Weno.hpp"
+#include "Evolution/Systems/NewtonianEuler/Tags.hpp"
+#include "Helpers/Evolution/DiscontinuousGalerkin/Limiters/TestHelpers.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeArray.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace {
+
+template <size_t VolumeDim>
+using VariablesMap = std::unordered_map<
+    std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>,
+    Variables<tmpl::list<NewtonianEuler::Tags::MassDensityCons,
+                         NewtonianEuler::Tags::MomentumDensity<VolumeDim>,
+                         NewtonianEuler::Tags::EnergyDensity>>,
+    boost::hash<std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>>>;
+
+template <size_t VolumeDim>
+std::unordered_map<
+    std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>,
+    typename NewtonianEuler::Limiters::Weno<VolumeDim>::PackagedData,
+    boost::hash<std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>>>
+make_neighbor_data_from_neighbor_vars(
+    const Mesh<VolumeDim>& mesh, const Element<VolumeDim>& element,
+    const std::array<double, VolumeDim>& element_size,
+    const VariablesMap<VolumeDim>& neighbor_vars) noexcept {
+  const auto make_tuple_of_means =
+      [&mesh](const Variables<
+              tmpl::list<NewtonianEuler::Tags::MassDensityCons,
+                         NewtonianEuler::Tags::MomentumDensity<VolumeDim>,
+                         NewtonianEuler::Tags::EnergyDensity>>&
+                  vars_to_average) noexcept {
+        tuples::TaggedTuple<
+            ::Tags::Mean<NewtonianEuler::Tags::MassDensityCons>,
+            ::Tags::Mean<NewtonianEuler::Tags::MomentumDensity<VolumeDim>>,
+            ::Tags::Mean<NewtonianEuler::Tags::EnergyDensity>>
+            result;
+        get(get<::Tags::Mean<NewtonianEuler::Tags::MassDensityCons>>(result)) =
+            mean_value(get(get<NewtonianEuler::Tags::MassDensityCons>(
+                           vars_to_average)),
+                       mesh);
+        for (size_t d = 0; d < VolumeDim; ++d) {
+          get<::Tags::Mean<NewtonianEuler::Tags::MomentumDensity<VolumeDim>>>(
+              result)
+              .get(d) =
+              mean_value(get<NewtonianEuler::Tags::MomentumDensity<VolumeDim>>(
+                             vars_to_average)
+                             .get(d),
+                         mesh);
+        }
+        get(get<::Tags::Mean<NewtonianEuler::Tags::EnergyDensity>>(result)) =
+            mean_value(
+                get(get<NewtonianEuler::Tags::EnergyDensity>(vars_to_average)),
+                mesh);
+        return result;
+      };
+
+  std::unordered_map<
+      std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>,
+      typename NewtonianEuler::Limiters::Weno<VolumeDim>::PackagedData,
+      boost::hash<std::pair<Direction<VolumeDim>, ElementId<VolumeDim>>>>
+      neighbor_data{};
+
+  for (const auto& neighbor : element.neighbors()) {
+    const auto dir = neighbor.first;
+    const auto id = *(neighbor.second.cbegin());
+    const auto dir_and_id = std::make_pair(dir, id);
+    neighbor_data[dir_and_id].volume_data = neighbor_vars.at(dir_and_id);
+    neighbor_data[dir_and_id].means =
+        make_tuple_of_means(neighbor_vars.at(dir_and_id));
+    neighbor_data[dir_and_id].mesh = mesh;
+    neighbor_data[dir_and_id].element_size = element_size;
+  }
+
+  return neighbor_data;
+}
+
+template <size_t VolumeDim>
+void test_limiter_work(const Scalar<DataVector>& input_density,
+                       const tnsr::I<DataVector, VolumeDim>& input_momentum,
+                       const Scalar<DataVector>& input_energy,
+                       const Mesh<VolumeDim>& mesh,
+                       const std::array<double, VolumeDim>& element_size,
+                       const EquationsOfState::EquationOfState<false, 1>& eos,
+                       const VariablesMap<VolumeDim>& neighbor_vars) noexcept {
+  const auto element = TestHelpers::Limiters::make_element<VolumeDim>();
+  const auto neighbor_data = make_neighbor_data_from_neighbor_vars(
+      mesh, element, element_size, neighbor_vars);
+
+  auto density_generic = input_density;
+  auto momentum_generic = input_momentum;
+  auto energy_generic = input_energy;
+  const double neighbor_linear_weight = 0.001;
+  const double tvb_constant = 0.0;  // TODO default this
+  const Limiters::Weno<
+      VolumeDim, tmpl::list<NewtonianEuler::Tags::MassDensityCons,
+                            NewtonianEuler::Tags::MomentumDensity<VolumeDim>,
+                            NewtonianEuler::Tags::EnergyDensity>>
+      weno_generic(Limiters::WenoType::SimpleWeno, neighbor_linear_weight,
+                   tvb_constant);
+  const bool activated_generic = weno_generic(
+      make_not_null(&density_generic), make_not_null(&momentum_generic),
+      make_not_null(&energy_generic), mesh, element, element_size,
+      neighbor_data);
+
+  auto density_specialized = input_density;
+  auto momentum_specialized = input_momentum;
+  auto energy_specialized = input_energy;
+  auto diagnostics = make_with_value<Scalar<DataVector>>(input_density, 0.0);
+  const NewtonianEuler::Limiters::Weno<VolumeDim> weno_specialized(
+      Limiters::WenoType::SimpleWeno, neighbor_linear_weight, tvb_constant);
+  const bool activated_specialized = weno_specialized(
+      make_not_null(&density_specialized), make_not_null(&momentum_specialized),
+      make_not_null(&energy_specialized), make_not_null(&diagnostics), mesh,
+      element, element_size, eos, neighbor_data);
+
+  CHECK(activated_generic);
+  CHECK(diagnostics == make_with_value<Scalar<DataVector>>(input_density, 1.0));
+  CHECK(activated_generic == activated_specialized);
+  CHECK_ITERABLE_APPROX(density_generic, density_specialized);
+  CHECK_ITERABLE_APPROX(momentum_generic, momentum_specialized);
+  CHECK_ITERABLE_APPROX(energy_generic, energy_specialized);
+}
+
+void test_weno_limiter_1d() noexcept {
+  INFO("Test NewtonianEuler::Limiters::Weno limiter in 1D");
+  const auto mesh =
+      Mesh<1>(3, Spectral::Basis::Legendre, Spectral::Quadrature::GaussLobatto);
+  const auto logical_coords = logical_coordinates(mesh);
+  const auto element_size = make_array<1>(0.5);
+  const EquationsOfState::PolytropicFluid<false> polytropic_eos{1., 2.};
+
+  const auto& x = get<0>(logical_coords);
+  const auto mass_density_cons = [&x]() noexcept {
+    return Scalar<DataVector>{{{1.0 + 0.2 * x + 0.05 * square(x)}}};
+  }();
+  const auto momentum_density = [&x]() noexcept {
+    return tnsr::I<DataVector, 1>{{{0.2 - 0.3 * x}}};
+  }();
+  const auto energy_density = [&mass_density_cons, &momentum_density,
+                               &polytropic_eos]() noexcept {
+    const auto& rho = get(mass_density_cons);
+    CAPTURE(rho);
+    const DataVector energy =
+        rho * get(polytropic_eos.specific_internal_energy_from_density(
+                  mass_density_cons)) +
+        0.5 * get(dot_product(momentum_density, momentum_density)) / rho;
+    return Scalar<DataVector>{{{energy}}};
+  }();
+
+  VariablesMap<1> neighbor_vars;
+  const std::array<std::pair<Direction<1>, ElementId<1>>, 2> dir_keys = {
+      {{Direction<1>::lower_xi(), ElementId<1>(1)},
+       {Direction<1>::upper_xi(), ElementId<1>(2)}}};
+  for (const auto& id_pair : dir_keys) {
+    neighbor_vars[id_pair].initialize(mesh.number_of_grid_points());
+  }
+
+  using rho = NewtonianEuler::Tags::MassDensityCons;
+  get(get<rho>(neighbor_vars[dir_keys[0]])) = 0.4;
+  get(get<rho>(neighbor_vars[dir_keys[1]])) = 1.1;
+
+  using rhou = NewtonianEuler::Tags::MomentumDensity<1>;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[0]])) = 0.2;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[1]])) = -0.1;
+
+  using eps = NewtonianEuler::Tags::EnergyDensity;
+  get(get<eps>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[1]])) = 0.0;
+
+  test_limiter_work(mass_density_cons, momentum_density, energy_density, mesh,
+                    element_size, polytropic_eos, neighbor_vars);
+}
+
+void test_weno_limiter_2d() noexcept {
+  INFO("Test NewtonianEuler::Limiters::Weno limiter in 2D");
+  const auto mesh =
+      Mesh<2>(3, Spectral::Basis::Legendre, Spectral::Quadrature::GaussLobatto);
+  const auto logical_coords = logical_coordinates(mesh);
+  const auto element_size = make_array(0.5, 1.0);
+  const EquationsOfState::PolytropicFluid<false> polytropic_eos{1., 2.};
+
+  const auto& x = get<0>(logical_coords);
+  const auto& y = get<1>(logical_coords);
+  const auto mass_density_cons = [&x, &y]() noexcept {
+    return Scalar<DataVector>{
+        {{1.0 + 0.2 * x - 0.1 * y + 0.05 * x * square(y)}}};
+  }();
+  const auto momentum_density = [&x, &y]() noexcept {
+    tnsr::I<DataVector, 2> momentum;
+    get<0>(momentum) = 0.2 - 0.3 * x + 0.1 * y;
+    get<1>(momentum) = -0.1 + 0.03 * y - 0.01 * square(x) * square(y);
+    return momentum;
+  }();
+  const auto energy_density = [&mass_density_cons, &momentum_density,
+                               &polytropic_eos]() noexcept {
+    const auto& rho = get(mass_density_cons);
+    const DataVector energy =
+        rho * get(polytropic_eos.specific_internal_energy_from_density(
+                  mass_density_cons)) +
+        0.5 * get(dot_product(momentum_density, momentum_density)) / rho;
+    return Scalar<DataVector>{{{energy}}};
+  }();
+
+  VariablesMap<2> neighbor_vars;
+  const std::array<std::pair<Direction<2>, ElementId<2>>, 4> dir_keys = {
+      {{Direction<2>::lower_xi(), ElementId<2>(1)},
+       {Direction<2>::upper_xi(), ElementId<2>(2)},
+       {Direction<2>::lower_eta(), ElementId<2>(3)},
+       {Direction<2>::upper_eta(), ElementId<2>(4)}}};
+  for (const auto& id_pair : dir_keys) {
+    neighbor_vars[id_pair].initialize(mesh.number_of_grid_points());
+  }
+
+  using rho = NewtonianEuler::Tags::MassDensityCons;
+  get(get<rho>(neighbor_vars[dir_keys[0]])) = 0.4;
+  get(get<rho>(neighbor_vars[dir_keys[1]])) = 1.1;
+  get(get<rho>(neighbor_vars[dir_keys[2]])) = 2.1;
+  get(get<rho>(neighbor_vars[dir_keys[3]])) = 0.9;
+
+  using rhou = NewtonianEuler::Tags::MomentumDensity<2>;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[0]])) = 0.2;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[1]])) = -0.1;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[2]])) = 0.1;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[3]])) = 0.2;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[0]])) = -0.8;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[1]])) = -0.2;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[2]])) = 0.1;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[3]])) = -0.1;
+
+  using eps = NewtonianEuler::Tags::EnergyDensity;
+  get(get<eps>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[1]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[2]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[3]])) = 0.0;
+
+  test_limiter_work(mass_density_cons, momentum_density, energy_density, mesh,
+                    element_size, polytropic_eos, neighbor_vars);
+}
+
+void test_weno_limiter_3d() noexcept {
+  INFO("Test NewtonianEuler::Limiters::Weno limiter in 3D");
+  const auto mesh =
+      Mesh<3>(std::array<size_t, 3>{{3, 3, 4}}, Spectral::Basis::Legendre,
+              Spectral::Quadrature::GaussLobatto);
+  const auto logical_coords = logical_coordinates(mesh);
+  const auto element_size = make_array(0.5, 1.0, 0.8);
+  const EquationsOfState::PolytropicFluid<false> polytropic_eos{1., 2.};
+
+  const auto& x = get<0>(logical_coords);
+  const auto& y = get<1>(logical_coords);
+  const auto& z = get<2>(logical_coords);
+  const auto mass_density_cons = [&x, &y, &z]() noexcept {
+    return Scalar<DataVector>{{{1.0 + 0.2 * x - 0.1 * y + 0.4 * z}}};
+  }();
+  const auto momentum_density = [&x, &y, &z]() noexcept {
+    tnsr::I<DataVector, 3> momentum;
+    get<0>(momentum) = 0.3 + 0.1 * z;
+    get<1>(momentum) = -1.2 - 0.1 * y * z;
+    get<2>(momentum) = 0.2 * x + 0.2 * y + 0.1 * z + 0.2 * square(z);
+    return momentum;
+  }();
+  const auto energy_density = [&mass_density_cons, &momentum_density,
+                               &polytropic_eos]() noexcept {
+    const auto& rho = get(mass_density_cons);
+    const DataVector energy =
+        rho * get(polytropic_eos.specific_internal_energy_from_density(
+                  mass_density_cons)) +
+        0.5 * get(dot_product(momentum_density, momentum_density)) / rho;
+    return Scalar<DataVector>{{{energy}}};
+  }();
+
+  VariablesMap<3> neighbor_vars;
+  const std::array<std::pair<Direction<3>, ElementId<3>>, 6> dir_keys = {
+      {{Direction<3>::lower_xi(), ElementId<3>(1)},
+       {Direction<3>::upper_xi(), ElementId<3>(2)},
+       {Direction<3>::lower_eta(), ElementId<3>(3)},
+       {Direction<3>::upper_eta(), ElementId<3>(4)},
+       {Direction<3>::lower_zeta(), ElementId<3>(5)},
+       {Direction<3>::upper_zeta(), ElementId<3>(6)}}};
+  for (const auto& id_pair : dir_keys) {
+    neighbor_vars[id_pair].initialize(mesh.number_of_grid_points());
+  }
+
+  using rho = NewtonianEuler::Tags::MassDensityCons;
+  get(get<rho>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get(get<rho>(neighbor_vars[dir_keys[1]])) = 0.0;
+  get(get<rho>(neighbor_vars[dir_keys[2]])) = 0.1;
+  get(get<rho>(neighbor_vars[dir_keys[3]])) = 1.2;
+  get(get<rho>(neighbor_vars[dir_keys[4]])) = 1.1;
+  get(get<rho>(neighbor_vars[dir_keys[5]])) = 0.9;
+
+  using rhou = NewtonianEuler::Tags::MomentumDensity<3>;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[0]])) = 0.2;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[1]])) = -0.1;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[2]])) = 0.1;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[3]])) = 0.2;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[4]])) = 0.0;
+  get<0>(get<rhou>(neighbor_vars[dir_keys[5]])) = 0.0;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[1]])) = 0.0;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[2]])) = 0.1;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[3]])) = 0.2;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[4]])) = -0.1;
+  get<1>(get<rhou>(neighbor_vars[dir_keys[5]])) = -0.2;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[1]])) = 0.0;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[2]])) = 0.0;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[3]])) = 0.0;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[4]])) = 1.1;
+  get<2>(get<rhou>(neighbor_vars[dir_keys[5]])) = 0.9;
+
+  using eps = NewtonianEuler::Tags::EnergyDensity;
+  get(get<eps>(neighbor_vars[dir_keys[0]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[1]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[2]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[3]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[4]])) = 0.0;
+  get(get<eps>(neighbor_vars[dir_keys[5]])) = 0.0;
+
+  test_limiter_work(mass_density_cons, momentum_density, energy_density, mesh,
+                    element_size, polytropic_eos, neighbor_vars);
+}
+
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.Evolution.Systems.NewtonianEuler.Limiters.Weno",
+                  "[Limiters][Unit]") {
+  // TODO(FH): Write a complete test
+  // Check cons-var weno matches the "generic" weno
+  test_weno_limiter_1d();
+  test_weno_limiter_2d();
+  test_weno_limiter_3d();
+}
