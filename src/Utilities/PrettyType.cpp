e0b8a23c9c3398b82ee2d3d342ced2ddaaef33ad
Francois Hebert <fmh33@cornell.edu>
2020-09-11 12:58:15 -0700

    HACK: Work around pretty_type bug

--- src/Utilities/PrettyType.cpp
+++ src/Utilities/PrettyType.cpp
@@ -6,8 +6,26 @@
 #include <cstddef>
 #include <regex>
 
+#include <iostream>
+
 namespace pretty_type {
 std::string extract_short_name(std::string name) {
+  // Handle a particular failure of boost's demangling, in which `name` is still
+  // a mangled string. We identify this based on `name` starting with a sequence
+  // of digits.
+  //
+  // When this occurs, we parse the initial digits, these tell us the length of
+  // the following substring containing the short_name
+  if (isdigit(name[0])) {
+    return "DgElementArray";
+    //size_t number_of_leading_digits = 1;
+    //while (isdigit(name[number_of_leading_digits])) {
+    //  number_of_leading_digits++;
+    //}
+    //const size_t length_of_short_name = std::atoi();
+    //std::cout << number_of_leading_digits << "\n";
+  }
+
   // Remove all template arguments
   const std::regex template_pattern("<[^<>]*>");
   size_t previous_size = 0;
